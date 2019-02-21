77781a951829acaf1276e67cad7ebcb2097a5147
zeen247 <zeendikwa18@gmail.com>
2019-02-21 18:44:17 +0100

    zeendis

---
+++ js/script.js
@@ -0,0 +1,78 @@
+//var uname1 = "Simi",
+//    pword1 = "hacky",
+//    name1 = "leah";
+////second username
+//var uname2 = "jin",
+//    pword2 = "dean",
+//    name2 = "jane";
+//
+//var uname = prompt("enter username");
+//var pword = prompt("enter username");
+//// chk credentials validity
+//if (uname1 === uname1 && pword1 === pword) {
+//    document.write("<h1>welcome " + name1 + "</h1>")
+//    //    console.log('hi')
+//} else if (uname1 === uname1 && pword1 === pword) {
+//    document.write("<h1>welcome " + name2 + "</h1>")
+//
+//} else {
+//    alert('yOu are incorrect')``````````
+//}
+
+//
+//var secretnumber=MaTH.floor (Math.random()*11;
+//var guess = Number(prompt("guess a number between 0 and 10"));
+//if (guess===secretnumber){
+//    document.write("<h1> u get sense")
+//} else if (guess>secretnumber){alert("abeg go school");
+//    } else{ alert("abeg try your luck again")}
+//ternary operators
+//var x;
+//var time="9am";
+//if(time==="10am"){x="good morning"}else{x="good bye"}
+//var x= (time==="10am") "goodmorning":"good bye"
+////Loops\{dont repeat ursef}
+//var x='1'
+//while(x<11){
+//    console.log(x);
+//    x++
+//}
+//var num =
+//    var x = 0
+//var y = 1
+//while num >= x
+//z = x + y
+//consol.log(x)
+// annoying goat
+``//question = prompt("are we there yet");
+//while (question.index of("yes") ===-1 and question.index of ("yeah")===-1)
+//{question=prompt(questions (math.floor(math.random ()*4)));}
+//document.write("finally home")
+ var str="hello";
+ for(var i=0; i<str.length;i++){
+     console.log(str[i]);
+ }
+for( vari=0;i<16;i+=8){
+    console.log(i);
+}
+var str="ahceelwlxo";
+for(var i = 1;i<str.length; i+=2)
+{console.log(str[i])
+}
+//function
+function rhymes(){
+   console.log("twinkle twinkle"); 
+    console.log("who are u");
+    console.log("and it shines all along"); 
+}
+rhymes();
+//arguments
+function area(length,breadth){
+    console.log(length*breadth);
+}
+area(3,8)
+function greet(per1,per2,per3){
+console.log("hello" + per1 )
+console.log("how far" + per2 )
+console.log("hey u" + per3 )
+}
